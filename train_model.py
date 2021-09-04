0872b4451e898a71ead5e11e439ff7d4ce51070a
Himank Goel <himankgoel03@gmail.com>
2021-09-04 21:02:26 +0530

    initial model

--- train_model.py
+++ train_model.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import (Activation, Conv2D, Dense, Flatten,
+                                     MaxPooling2D, Dropout)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 input_shape = (32, 32, 3)
@@ -38,3 +43,52 @@ validation_generator = validation_datagen.flow_from_directory(
     class_mode = 'categorical',
     shuffle = False)   
 
+model = Sequential()
+
+model.add(Conv2D(32, (5, 5), padding = "same", input_shape = input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+model.add(Conv2D(64, (5, 5), padding = "same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+model.add(Flatten())
+model.add(Dense(500))
+model.add(Activation("relu"))
+
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
+           
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = tf.keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+    
+print(model.summary())
+
+                   
+checkpoint = ModelCheckpoint("models/creditcard.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor='val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+callbacks = [earlystop, checkpoint]
+
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
